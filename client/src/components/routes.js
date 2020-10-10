6468baccea6f849b979925cde64ed5cadd680389
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-10-10 13:29:58 -0300

    Setitng up auth context

--- client/src/components/routes.js
+++ client/src/components/routes.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './containers/NavBar';
 import Dashboard from './containers/Dashboard';
 import Login from './containers/Login';
-import PrivateRoute from './PrivateRoute';
+import PrivateRoute from './PrivateRouting';
 
 const Routes = () => {
   return (
     <div>
       <Fragment>
         <NavBar />
-        <Switch>
-          <Route exact path='/register' component={Dashboard} />
-          <PrivateRoute exact path='/login' component={Login} />
-        </Switch>
+        <Router>
+          <Switch>
+            <Route exact path='/register' component={Dashboard} />
+            <PrivateRoute exact path='/login' component={Login} />
+          </Switch>
+        </Router>
       </Fragment>
     </div>
   );
